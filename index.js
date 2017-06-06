e0192d2c1d04bc930d2097e0d1e6bcb89314067a
Teodoro777 <teodoro.coluccio@gmail.com>
2017-06-06 21:19:23 +0200

    Aggiunta funzione distanza

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var coords = [ { x: 1, y: 2}, 
+  {x: 7, y: 7}
+];
+
 server.listen(3000);
 
 app.get('/', function (req, res) {
@@ -11,8 +15,17 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
   console.log('nuovo utente connesso');
+  
+  socket.emit('lista_coords', coords);
 
-  socket.on('pippo', function () {
-    console.log('un client sta cliccando');
+  socket.on('distanza', function (data) {
+    console.log(data);
+    var d = Math.sqrt(data.x*data.x + data.y * data.y);
+
+    /* Memorizza il dato del db */
+    coords.push(data);
+    console.log(coords);
+
+    socket.emit('distanza_result', { dist: d});
   });
 });
